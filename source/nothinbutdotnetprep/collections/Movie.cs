228959a42d36692be6f8cf828d682d89600430fe
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-05-16 14:51:47 -0600

    Updated

--- source/nothinbutdotnetprep/collections/Movie.cs
+++ source/nothinbutdotnetprep/collections/Movie.cs
@@ -2,70 +2,68 @@ using System;
 
 namespace nothinbutdotnetprep.collections
 {
-    public class Movie
+  public class Movie : IEquatable<Movie>
+  {
+    public string title { get; set; }
+    public ProductionStudio production_studio { get; set; }
+    public Genre genre { get; set; }
+    public int rating { get; set; }
+    public DateTime date_published { get; set; }
+
+    public override int GetHashCode()
     {
-        public string title { get; set; }
-        public ProductionStudio production_studio { get; set; }
-        public Genre genre { get; set; }
-        public int rating { get; set; }
-        public DateTime date_published { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            return obj is Movie && Equals((Movie) obj);
-        }
-
-        public bool Equals(Movie other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            return Equals(other.title, title) && Equals(other.production_studio, production_studio) && Equals(other.genre, genre) && other.rating == rating && other.date_published.Equals(date_published);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int result = (title != null ? title.GetHashCode() : 0);
-                result = (result*397) ^ (production_studio != null ? production_studio.GetHashCode() : 0);
-                result = (result*397) ^ (genre != null ? genre.GetHashCode() : 0);
-                result = (result*397) ^ rating;
-                result = (result*397) ^ date_published.GetHashCode();
-                return result;
-            }
-        }
-
-        public override string ToString()
-        {
-            return string.Format("{0} - {1}", title, nameOfProductionStudio());
-        }
-
-        private string nameOfProductionStudio()
-        {
-            if (production_studio == ProductionStudio.MGM)
-            {
-                return "MGM";
-            }
-            if (production_studio == ProductionStudio.Disney)
-            {
-                return "Disney";
-            }
-            if (production_studio == ProductionStudio.Dreamworks)
-            {
-                return "Dreamworks";
-            }
-            if (production_studio == ProductionStudio.Paramount)
-            {
-                return "Paramount";
-            }
-            if (production_studio == ProductionStudio.Pixar)
-            {
-                return "Pixar";
-            }
-            if (production_studio == ProductionStudio.Universal)
-            {
-                return "Universal";
-            }
-            return "Unknown";
-        }
+      return title.GetHashCode();
     }
+
+    public override string ToString()
+    {
+      return string.Format("{0} - {1}", title, nameOfProductionStudio());
+    }
+
+    string nameOfProductionStudio()
+    {
+      if (production_studio == ProductionStudio.MGM)
+      {
+        return "MGM";
+      }
+      if (production_studio == ProductionStudio.Disney)
+      {
+        return "Disney";
+      }
+      if (production_studio == ProductionStudio.Dreamworks)
+      {
+        return "Dreamworks";
+      }
+      if (production_studio == ProductionStudio.Paramount)
+      {
+        return "Paramount";
+      }
+      if (production_studio == ProductionStudio.Pixar)
+      {
+        return "Pixar";
+      }
+      if (production_studio == ProductionStudio.Universal)
+      {
+        return "Universal";
+      }
+      return "Unknown";
+    }
+
+    public override bool Equals(object obj)
+    {
+      return Equals(obj as Movie);
+    }
+
+    public bool Equals(Movie other)
+    {
+      if (other == null) return false;
+
+      return ReferenceEquals(null, other) || is_equal_to_non_null_instance_of(other);
+    }
+
+    bool is_equal_to_non_null_instance_of(Movie other)
+    {
+      return Equals(other.title, title);
+    }
+  }
 }
--- source/nothinbutdotnetprep/collections/MovieLibrary.cs
+++ source/nothinbutdotnetprep/collections/MovieLibrary.cs
@@ -13,31 +13,6 @@ namespace nothinbutdotnetprep.collections
       movies = list_of_movies;
     }
 
-    IEnumerable<Movie> sortBy(params Comparison<Movie>[] comparison)
-    {
-      var list = new List<Movie>(movies);
-
-      Comparison<Movie> comparer = (x, y) =>
-      {
-        for (var i = 0; i < comparison.Length; i++)
-        {
-          var result = comparison[i](x, y);
-          if (result != 0)
-          {
-            return result;
-          }
-        }
-        return 0;
-      };
-
-      list.Sort(comparer);
-
-      foreach (var movie in list)
-      {
-        yield return movie;
-      }
-    }
-
     public IEnumerable<Movie> all_movies()
     {
       return movies.one_at_a_time();
@@ -53,11 +28,6 @@ namespace nothinbutdotnetprep.collections
     bool already_contains(Movie movie)
     {
       return movies.Contains(movie);
-    }
-
-    public IEnumerable<Movie> sort_all_movies_by_title_descending
-    {
-      get { return sortBy((x, y) => x.title.CompareTo(y.title)*-1); }
     }
 
     public IEnumerable<Movie> all_movies_published_by_pixar()
@@ -80,44 +50,6 @@ namespace nothinbutdotnetprep.collections
           yield return movie;
         }
       }
-    }
-
-    public IEnumerable<Movie> sort_all_movies_by_title_ascending
-    {
-      get { return sortBy((x, y) => x.title.CompareTo(y.title)); }
-    }
-
-    public IEnumerable<Movie> sort_all_movies_by_movie_studio_and_year_published()
-    {
-      var rankings =
-        new[]
-        {
-          ProductionStudio.MGM,
-          ProductionStudio.Pixar,
-          ProductionStudio.Dreamworks,
-          ProductionStudio.Universal,
-          ProductionStudio.Disney,
-          ProductionStudio.Paramount,
-        };
-
-      Func<ProductionStudio, int> indexOf = x =>
-      {
-        for (var i = 0; i < rankings.Length; i++)
-        {
-          if (rankings[i] == x)
-          {
-            return i;
-          }
-        }
-        return -1;
-      };
-
-      Comparison<ProductionStudio> studioRankings = (x, y) => { return indexOf(x).CompareTo(indexOf(y)); };
-
-      return sortBy(
-        (x, y) => studioRankings(x.production_studio, y.production_studio),
-        (x, y) => x.date_published.Year.CompareTo(y.date_published.Year)
-        );
     }
 
     public IEnumerable<Movie> all_movies_not_published_by_pixar()
@@ -184,6 +116,74 @@ namespace nothinbutdotnetprep.collections
     public IEnumerable<Movie> sort_all_movies_by_date_published_ascending()
     {
       return sortBy((x, y) => x.date_published.CompareTo(y.date_published));
+    }
+
+    public IEnumerable<Movie> sort_all_movies_by_title_descending
+    {
+      get { return sortBy((x, y) => x.title.CompareTo(y.title)*-1); }
+    }
+
+    public IEnumerable<Movie> sort_all_movies_by_title_ascending
+    {
+      get { return sortBy((x, y) => x.title.CompareTo(y.title)); }
+    }
+
+    public IEnumerable<Movie> sort_all_movies_by_movie_studio_and_year_published()
+    {
+      var rankings =
+        new[]
+        {
+          ProductionStudio.MGM,
+          ProductionStudio.Pixar,
+          ProductionStudio.Dreamworks,
+          ProductionStudio.Universal,
+          ProductionStudio.Disney,
+          ProductionStudio.Paramount,
+        };
+
+      Func<ProductionStudio, int> indexOf = x =>
+      {
+        for (var i = 0; i < rankings.Length; i++)
+        {
+          if (rankings[i] == x)
+          {
+            return i;
+          }
+        }
+        return -1;
+      };
+
+      Comparison<ProductionStudio> studioRankings = (x, y) => { return indexOf(x).CompareTo(indexOf(y)); };
+
+      return sortBy(
+        (x, y) => studioRankings(x.production_studio, y.production_studio),
+        (x, y) => x.date_published.Year.CompareTo(y.date_published.Year)
+        );
+    }
+
+    IEnumerable<Movie> sortBy(params Comparison<Movie>[] comparison)
+    {
+      var list = new List<Movie>(movies);
+
+      Comparison<Movie> comparer = (x, y) =>
+      {
+        for (var i = 0; i < comparison.Length; i++)
+        {
+          var result = comparison[i](x, y);
+          if (result != 0)
+          {
+            return result;
+          }
+        }
+        return 0;
+      };
+
+      list.Sort(comparer);
+
+      foreach (var movie in list)
+      {
+        yield return movie;
+      }
     }
   }
 }
